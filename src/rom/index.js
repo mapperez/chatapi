26fd4322385eab6559f8a8d6a4b563de47aa024d
Maicol Ortiz <corematrixx@gmail.com>
2019-03-27 11:03:42 -0300

    avances bots

--- src/rom/index.js
+++ src/rom/index.js
@@ -1,5 +1,7 @@
 const moment = require('moment-timezone');
-const { io } = require('../../index')
+const {
+    io
+} = require('../../index')
 const request = require("request");
 
 //Modelos
@@ -9,7 +11,9 @@ const cliente = require('../model/cliente.model')
 
 
 const bodyParser = require('body-parser')
-const { mensajeABots } = require('../dialogflow')
+const {
+    mensajeABots
+} = require('../dialogflow')
 
 
 
@@ -24,7 +28,7 @@ async function SendMensajesClientesWsp(data) {
             method: "POST",
             json: data
         },
-        function(error, response, body) {
+        function (error, response, body) {
             if (error) return console.error("HTTP Error", error);
             console.log(data);
         }
@@ -42,32 +46,19 @@ async function romAddMessage(msg) {
 
     // Mensajes del cliente
     if (msg.fromMe == false) {
-        console.log('1. MENSAJE DE CLIENTE');
-       let resultado = await gestionaMensajeCliente(msg);
-
-          console.log("##################################################");
-          console.log("RESULTADO DE GESTIONA MENSAJE CLIENTE");
-     
-          console.log("##################################################");
+        console.log('1. GESTIONA MENSAJE CLIENTE');
+        let resultado = await gestionaMensajeCliente(msg);
 
 
-        //   //VERIFICAMOS CONVERSACION CON EL BOTS
-        //   console.log("##################################################");
+        console.log('2. BOTS PROCESA MENSAJE DE CLIENTE');
 
-        //   console.log(msg);
-        //   console.log(msg.body);
-
-        //   console.log("##################################################");
-        //   const dataBots = {
-        //       "_id"    : "1212",
-        //       "mensaje": msg.body,
-        //       "type": msg.type    
-        //   }
-        //   console.log(dataBots);
-        //   console.log("-------------------------------------------");
-
-        //   let respuestaBots = await procesaMensajeBots(dataBots);
-        //   console.log(respuestaBots);
+          const dataBots = {
+              "_id"    : resultado.rom._id,
+              "mensaje": resultado.ultimoMensaje,             
+          }
+          console.log(dataBots); 
+          let respuestaBots = await procesaMensajeBots(dataBots);
+          console.log(respuestaBots);
 
 
     } else {
@@ -78,25 +69,22 @@ async function romAddMessage(msg) {
 
 }
 
-async function procesaMensajeBots(data){
+async function procesaMensajeBots(data) {
 
     console.log("Entra procesaMensajeBots");
     console.log(data)
-  
 
-    let mensaje = data.mensaje
+
+    if(data.mensaje.type == "chat"){
+        let mensaje = data.mensaje.body
         let sessionId = data._id
         let respuesta = await mensajeABots(mensaje, sessionId)
-        return respuesta;
 
-    // if(data.type == "chat"){
-    //     let mensaje = data.mensaje
-    //     let sessionId = data._id
-    //     let respuesta = await mensajeABots(mensaje, sessionId)
-    //     return respuesta;
-    // }else{
-    //     return  false;
-    // }
+        console.log(respuesta)
+        return true;
+    }else{
+        return  false;
+    }
 
 }
 
@@ -104,138 +92,155 @@ function getRoms() {
     //Fecha Hoy
     const today = moment(new Date).tz("America/Santiago")
     const fechaHoy = today.format('YYYY-MM-DD')
-    return rom.find({ open: true, fecha: { $gte: fechaHoy } })
+    return rom.find({
+        open: true,
+        fecha: {
+            $gte: fechaHoy
+        }
+    })
 }
 
-async function gestionaMensajeCliente(msg){
+async function gestionaMensajeCliente(msg) {
     const today = moment(new Date).tz("America/Santiago")
     const fechaHoy = today.format('YYYY-MM-DD')
     let clienteSinNombre = msg.author.replace('@c.us', '')
 
-        // Buscar Rom Activa 
-        let romActual = await rom.findOne({ chatId: msg.chatId, open: true, 
-            $or: [{
-                estado: "Espera"
-            }, {
-                estado: "Activa"
-            }] 
-         }).sort({updatedAt: -1});
+    // Buscar Rom Activa 
+    let romActual = await rom.findOne({
+        chatId: msg.chatId,
+        open: true,
+        $or: [{
+            estado: "Espera"
+        }, {
+            estado: "Activa"
+        }]
+    }).sort({
+        updatedAt: -1
+    });
 
 
-        let mjs = [];
+    let mjs = [];
 
-        console.log("ROM ACTUAL")
-        console.log(romActual);
-        console.log("-------------------------------------------");
-
-
-
-        // SE VERIFICA SI EXISTE LA CONVERSACION O LA CREA
-        if (!romActual) {
-
-            const cliok =  await getDatosCliente(msg)
-            console.log('GET DATOS CLIENTES');
-            console.log(cliok);
-
-            //Crear Rom
-            mjs = []
-            mjs.push(msg)
-
-            let NewRom = {
-                    chatId: msg.chatId,
-                    fecha: fechaHoy,
-                    open: true,
-                    mensajes: mjs,
-                    cliente: cliok,
-                    estado: 'Espera'
-                }
-                // Guarda rom
-
-            rom.create(NewRom).then(async(resp, err) => {
-                if (err) {
-                    console.log('Error al crear rom');
-                }
-                console.log(`Se crea nuevo rom con mensaje del cliente: ${resp._id}`);
-
-                // Notificar conectados
-                console.log('Emite rom vigentes a los clientes del chat');
-                let roms = await rom.find({ 
-                    open: true,
-                    $or: [{
-                        estado: "Espera"
-                    }, {
-                        estado: "Activa"
-                    }] 
-                 }).sort({updatedAt: -1});
-
-                io.emit('sendClientNew', roms)
+    console.log("ROM ACTUAL")
+    console.log(romActual);
+    console.log("-------------------------------------------");
 
 
-            
 
+    // SE VERIFICA SI EXISTE LA CONVERSACION O LA CREA
+    if (!romActual) {
 
-            }).catch(err => {
+        const cliok = await getDatosCliente(msg)
+        console.log('GET DATOS CLIENTES');
+        console.log(cliok);
+
+        //Crear Rom
+        mjs = []
+        mjs.push(msg)
+
+        let NewRom = {
+            chatId: msg.chatId,
+            fecha: fechaHoy,
+            open: true,
+            mensajes: mjs,
+            cliente: cliok,
+            estado: 'Espera'
+        }
+        // Guarda rom
+
+        rom.create(NewRom).then(async (resp, err) => {
+            if (err) {
                 console.log('Error al crear rom');
-            })
-
-
-        } else {
-
-            console.log('Existe Rom creado para cliente');
-            console.log("ChatId WebHook: " + msg.chatId);
-            console.log("ChatId : " + romActual.chatId);
-            console.log("RomID : " + romActual._id);
-
-            let mensajeActual = msg;
-
-         
-
-            // Rom existe y esta abierto
-            mjs = []
-            mjs = romActual.mensajes
-            mjs.push(msg)
-            romActual.mensajes = []
-            romActual.mensajes = mjs
-
-            let upRom = {            
-                mensajes: mjs
             }
+            console.log(`Se crea nuevo rom con mensaje del cliente: ${resp._id}`);
 
-            // ACTUALIZAMOS MENSAJES
-            console.log(`findOneAndUpdate ID: ${romActual._id}`);
-            let resUpdateMensajes = await rom.findOneAndUpdate({_id: romActual._id}, upRom, {new: true, runValidators: true});
-
-          
-           
-            // RESCATAMOS LAS CONVERSACIONES
-            console.log(`Se crea nuevo mensaje al Rom Id : ${resUpdateMensajes._id}`);
+            // Notificar conectados
             console.log('Emite rom vigentes a los clientes del chat');
-            let roms = await rom.find({ 
+            let roms = await rom.find({
                 open: true,
                 $or: [{
                     estado: "Espera"
                 }, {
                     estado: "Activa"
                 }]
-                }).sort({updatedAt: -1});
-            io.emit('sendClientMensaje', roms)
-       
+            }).sort({
+                updatedAt: -1
+            });
 
-            // console.log(resultado);
-            console.log("LLEGA AL FINAL DEL CODIGO DE findOneAndUpdate")
-
-            console.log(mensajeActual)
-           
-            return  resUpdateMensajes
+            io.emit('sendClientNew', roms)
 
 
 
+
+
+        }).catch(err => {
+            console.log('Error al crear rom');
+        })
+
+
+    } else {
+
+        console.log('Existe Rom creado para cliente');
+        console.log("ChatId WebHook: " + msg.chatId);
+        console.log("ChatId : " + romActual.chatId);
+        console.log("RomID : " + romActual._id);
+
+        let mensajeActual = msg;
+
+
+
+        // Rom existe y esta abierto
+        mjs = []
+        mjs = romActual.mensajes
+        mjs.push(msg)
+        romActual.mensajes = []
+        romActual.mensajes = mjs
+
+        let upRom = {
+            mensajes: mjs
         }
+
+        // ACTUALIZAMOS MENSAJES
+        console.log(`findOneAndUpdate ID: ${romActual._id}`);
+        let resUpdateMensajes = await rom.findOneAndUpdate({
+            _id: romActual._id
+        }, upRom, {
+            new: true,
+            runValidators: true
+        });
+
+
+
+        // RESCATAMOS LAS CONVERSACIONES
+        console.log(`Se crea nuevo mensaje al Rom Id : ${resUpdateMensajes._id}`);
+        console.log('Emite rom vigentes a los clientes del chat');
+        let roms = await rom.find({
+            open: true,
+            $or: [{
+                estado: "Espera"
+            }, {
+                estado: "Activa"
+            }]
+        }).sort({
+            updatedAt: -1
+        });
+        io.emit('sendClientMensaje', roms)
+        console.log("LLEGA AL FINAL DEL CODIGO DE findOneAndUpdate")
+
+        const retorna = {
+            ultimoMensaje: mensajeActual,
+            rom: resUpdateMensajes
+        }
+        return retorna
+
+
+
+    }
 
 }
 
 
-async function getDatosCliente(msg){
+async function getDatosCliente(msg) {
     let clienteSinNombre = msg.author.replace('@c.us', '')
 
 
@@ -245,7 +250,9 @@ async function getDatosCliente(msg){
     let telefono = msg.author.replace('@c.us', '')
 
     //Buscar cliente
-    let cli = await cliente.findOne({ 'telefonos.telefono': `+${telefono}` }, {
+    let cli = await cliente.findOne({
+        'telefonos.telefono': `+${telefono}`
+    }, {
         telefonos: {
             $elemMatch: {
                 telefono: `+${telefono}`
@@ -253,90 +260,99 @@ async function getDatosCliente(msg){
         }
     });
 
-     if (!cli) {
+    if (!cli) {
 
-         console.log('No existe cliente asociado y asocia cliente nuevo');
-         cliok = {
-             rut: '',
-             nombre: clienteSinNombre,
-             apellido: '',
-             correos: '',
-             telefonos: '',
-             direcciones: '',
-             cod_region: '',
-             provincia: '',
-             ciudad: '',
-             clasificacionFinanciera: '',
-             tipoCartera: '',
-             clasificacion: '',
-             condicionPago: '',
-             credito: '',
-             creditoUtilizado: '',
-             formaPago: '',
-             giros: '',
-             segmento: '',
-             subSegmento: ''
-         }
+        console.log('No existe cliente asociado y asocia cliente nuevo');
+        cliok = {
+            rut: '',
+            nombre: clienteSinNombre,
+            apellido: '',
+            correos: '',
+            telefonos: '',
+            direcciones: '',
+            cod_region: '',
+            provincia: '',
+            ciudad: '',
+            clasificacionFinanciera: '',
+            tipoCartera: '',
+            clasificacion: '',
+            condicionPago: '',
+            credito: '',
+            creditoUtilizado: '',
+            formaPago: '',
+            giros: '',
+            segmento: '',
+            subSegmento: ''
+        }
 
-     } else {
-         console.log('Busca cliente por su Id');
-         cliok = await cliente.findById(cli._id)
-     }
-     return  cliok;
+    } else {
+        console.log('Busca cliente por su Id');
+        cliok = await cliente.findById(cli._id)
+    }
+    return cliok;
 }
 //-----------------------------------------------------------------------------------------
 
-async function gestionaMensajeCrmChat(msg){
+async function gestionaMensajeCrmChat(msg) {
 
-            // Notificar conectados
-            console.log('2. MENSAJE DE EJECUTIVO');
+    // Notificar conectados
+    console.log('2. MENSAJE DE EJECUTIVO');
 
-            let romUpdate = await rom.findOne({ chatId: msg.chatId, open: true,  
+    let romUpdate = await rom.findOne({
+        chatId: msg.chatId,
+        open: true,
+        $or: [{
+            estado: "Espera"
+        }, {
+            estado: "Activa"
+        }]
+    });
+    console.log("ChatId WebHook: " + msg.chatId);
+    console.log("ChatId : " + romUpdate.chatId);
+    console.log("RomID : " + romUpdate._id);
+
+
+    if (romUpdate) {
+        // Rom existe y esta abierto
+        mjs = []
+        mjs = romUpdate.mensajes
+        mjs.push(msg)
+        romUpdate.mensajes = []
+        romUpdate.mensajes = mjs
+
+        let upRom = {
+            mensajes: mjs
+        }
+
+        rom.findOneAndUpdate({
+            _id: romUpdate._id
+        }, upRom, {
+            new: true,
+            runValidators: true
+        }, async (err, item) => {
+            if (err) {
+                console.log(err);
+            }
+
+            console.log(`Se crea nuevo mensaje al Rom Id : ${item._id}`);
+            console.log('Emite rom vigentes a los clientes del chat');
+            let roms = await rom.find({
+                open: true,
                 $or: [{
-                estado: "Espera"
+                    estado: "Espera"
                 }, {
                     estado: "Activa"
                 }]
-             });
-            console.log("ChatId WebHook: " + msg.chatId);
-            console.log("ChatId : " + romUpdate.chatId);
-            console.log("RomID : " + romUpdate._id);
-    
-    
-            if (romUpdate) {
-                // Rom existe y esta abierto
-                mjs = []
-                mjs = romUpdate.mensajes
-                mjs.push(msg)
-                romUpdate.mensajes = []
-                romUpdate.mensajes = mjs
-    
-                let upRom = {
-                    mensajes: mjs
-                }
-    
-                rom.findOneAndUpdate({ _id: romUpdate._id }, upRom, { new: true, runValidators: true }, async(err, item) => {
-                    if (err) {
-                        console.log(err);
-                    }
-    
-                    console.log(`Se crea nuevo mensaje al Rom Id : ${item._id}`);
-                    console.log('Emite rom vigentes a los clientes del chat');
-                    let roms = await rom.find({ 
-                        open: true,
-                        $or: [{
-                            estado: "Espera"
-                        }, {
-                            estado: "Activa"
-                        }]
-                     }).sort({updatedAt: -1});
-                    io.emit('sendClientMensaje', roms)
-    
-                }).catch(err => {
-                    console.log('Error el actualizar');
-                }) 
-    
-            }    
+            }).sort({
+                updatedAt: -1
+            });
+            io.emit('sendClientMensaje', roms)
+
+        }).catch(err => {
+            console.log('Error el actualizar');
+        })
+
+    }
 
 }
 // ---------------------------------------------------------------------------------------
@@ -344,6 +360,5 @@ async function gestionaMensajeCrmChat(msg){
 
 module.exports = {
     romAddMessage,
-    SendMensajesClientesWsp,
-    procesaMensajeBots
+    SendMensajesClientesWsp
 }
